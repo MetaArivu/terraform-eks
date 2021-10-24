1794c872d85351710647f1e7354ad1ec5a932ab2
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 22:13:08 +0530

    Update main.tf

--- examples/02-aws-vpc/main.tf
+++ examples/02-aws-vpc/main.tf
@@ -1,10 +1,10 @@
-resource "aws_vpc" "aws-ariu-vpc" {
+resource "aws_vpc" "arivu-aws-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = "true" #gives you an internal domain name
     enable_dns_hostnames = "true" #gives you an internal host name
     enable_classiclink = "false"
     instance_tenancy = "default"    
     tags = {
-        Name = "aws-ariu-vpc"
+        Name = "arivu-aws-vpc"
 }
 }
