5c8838105997fb7f5560cbce1c04d01b7100ff77
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 22:17:07 +0530

    Update vpc-subnet.tf

--- examples/04-aws-vpc-subnet-ec2-security-grp/vpc-subnet.tf
+++ examples/04-aws-vpc-subnet-ec2-security-grp/vpc-subnet.tf
@@ -1,20 +1,20 @@
-resource "aws_vpc" "ariu-vpc" {
+resource "aws_vpc" "arivu-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = "true" 
     enable_dns_hostnames = "true" 
     enable_classiclink = "false"
     instance_tenancy = "default"    
     tags = {
-        Name = "ariu-vpc"
+        Name = "arivu-vpc"
 }
 }
 
-resource "aws_subnet" "ariu-subnet-public-3" {
-    vpc_id = "${aws_vpc.ariu-vpc.id}"
+resource "aws_subnet" "arivu-subnet-public-3" {
+    vpc_id = "${aws_vpc.arivu-vpc.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-2a"
     tags = {
-        Name = "ariu-subnet-public-3"
+        Name = "arivu-subnet-public-3"
     }
 }
