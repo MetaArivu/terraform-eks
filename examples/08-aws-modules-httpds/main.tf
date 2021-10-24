87b635fd29eb1ce834daae3f07c73f4dd48d2e4a
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 22:42:35 +0530

    Update main.tf

--- examples/08-aws-modules-httpds/main.tf
+++ examples/08-aws-modules-httpds/main.tf
@@ -1,12 +1,12 @@
-module "ariu_aws_vpc" {
+module "arivu_aws_vpc" {
    source = "./modules/vpc"
    aws-vpc-cidr = var.aws-vpc-cidr
    aws-subnet-cidr = var.aws-subnet-cidr
-   vpc_id      = "${module.ariu_aws_vpc.vpc_id}"  
+   vpc_id      = "${module.arivu_aws_vpc.vpc_id}"  
 }
 
-module "ariu_aws_ec2" {
+module "arivu_aws_ec2" {
   source = "./modules/ec2"
-  subnet_id     = module.ariu_aws_vpc.subnet_id
-  ssh-allowed = module.ariu_aws_vpc.ssh-allowed
+  subnet_id     = module.arivu_aws_vpc.subnet_id
+  sshbyall = module.arivu_aws_vpc.sshbyall
 }
