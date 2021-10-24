c365871484f1d15e24d6ef2d25c06ffe518a8e64
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 22:16:12 +0530

    Update main.tf

--- examples/04-aws-vpc-subnet-ec2-security-grp/main.tf
+++ examples/04-aws-vpc-subnet-ec2-security-grp/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "aws_ariu_ins" {
+resource "aws_instance" "arivu_aws_ins" {
 
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
 
     # VPC
-    subnet_id = "${aws_subnet.ariu-subnet-public-3.id}"
+    subnet_id = "${aws_subnet.arivu-subnet-public-3.id}"
 
     # Security Group
     vpc_security_group_ids = ["${aws_security_group.sshbyall.id}"]
