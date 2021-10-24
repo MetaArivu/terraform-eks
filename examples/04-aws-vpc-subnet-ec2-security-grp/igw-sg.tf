8712497c00e0b8bcd4445ce97ea3194cc220aa57
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 20:28:07 +0530

    Create igw-sg.tf

---
+++ examples/04-aws-vpc-subnet-ec2-security-grp/igw-sg.tf
@@ -0,0 +1,59 @@
+# create Internet Gateway for vpc to connect with internet
+resource "aws_internet_gateway" "ariu-igw" {
+    vpc_id = "${aws_vpc.ariu-vpc.id}"
+
+    tags = {
+        Name = "ariu-igw"
+    }
+}
+
+# public subnets route table
+resource "aws_route_table" "ariu-public-rt" {
+    vpc_id = "${aws_vpc.ariu-vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0" 
+        gateway_id = "${aws_internet_gateway.ariu-igw.id}" 
+    }
+
+    tags = {
+        Name = "ariu-public-rt"
+    }
+}
+
+# route table association for the public subnets
+resource "aws_route_table_association" "ariu-rta-public-subnet-1" {
+    subnet_id = "${aws_subnet.ariu-subnet-public-1.id}"
+    route_table_id = "${aws_route_table.ariu-public-rt.id}"
+}
+
+# security group
+resource "aws_security_group" "sshbyall" {
+
+    vpc_id = "${aws_vpc.ariu-vpc.id}"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "sshbyall"
+    }
+}
