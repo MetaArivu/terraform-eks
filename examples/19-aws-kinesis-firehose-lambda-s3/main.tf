ea797a55465746e24d5464e80e32ca8d36df17d6
deepali-arvind <deepali.arvind@metamagic.in>
2021-12-14 16:18:10 +0530

    lambda-kfs-lambda

--- examples/19-aws-kinesis-firehose-lambda-s3/main.tf
+++ examples/19-aws-kinesis-firehose-lambda-s3/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
   name        = "terraform-kinesis-firehose-extended-s3-test-stream"
   destination = "extended_s3"
@@ -45,9 +46,8 @@ resource "aws_iam_role" "firehose_role" {
 }
 EOF
 }
-
 resource "aws_iam_role" "lambda_iam" {
-  name = "lambda_iam"
+  name = "04-stock-lambda-enhancer"
 
   assume_role_policy = <<EOF
 {
@@ -67,9 +67,21 @@ EOF
 }
 
 resource "aws_lambda_function" "lambda_processor" {
-  filename      = "lambda.zip"
-  function_name = "firehose_lambda_processor"
+  filename      = "04-stock-lambda-enhancer-1.0-aws.jar"
+  function_name = "04-stock-lambda-enhancer"
   role          = aws_iam_role.lambda_iam.arn
-  handler       = "exports.handler"
-  runtime       = "nodejs12.x"
+  handler       = "com.metaarivu.AWSLambdaHandler"
+  runtime       = "java8"
+  timeout = 60
+  memory_size = 256    
+  
 }
+
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "04-stock-lambda-enhancer"
+  role   = aws_iam_role.lambda_iam.id
+  policy = file("04-stock-lambda-enhancer.json")
+}
+
+
